dc2de814c8b0722ddcd37e153b5fb67615ebcf97
filthyweeaboo <lolipopfun@gmail.com>
2014-01-05 02:37:12 +0000

    Create youtube.sh

---
+++ youtube.sh
@@ -0,0 +1,578 @@
+#!/bin/bash
+echo 'Youtube/soundcloud bulk downloader'
+mkdir DL
+cd DL
+touch temp.sh
+echo '#!/bin/bash/' > temp.sh
+read -p "Insert URL
+" yt ;
+
+echo youtube-dl $yt >> temp.sh
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+echo youtube-dl $yt >> temp.sh
+
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
+######################################
+read -p "Add another one [y/n]?" aa ;
+	if [ $aa = y ]; then
+	read -p "Insert URL
+" yt ;
+		echo youtube-dl $yt >> temp.sh
+	else
+		sh temp.sh
+		exit 1
+	fi
